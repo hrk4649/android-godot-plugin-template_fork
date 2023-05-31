904b4bf5430dabae63f6c4ea0e3e8d1deb9af6b0
Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
2023-06-01 07:01:14 +0900

    build on Android Studio Flamingo

--- app/src/main/java/com/badlight/templateplugin/AndroidGodotPluginTemplate.kt
+++ app/src/main/java/com/badlight/templateplugin/AndroidGodotPluginTemplate.kt
@@ -2,8 +2,11 @@ package com.badlight.templateplugin
 
 import android.telephony.SmsManager
 import android.util.Log
+import androidx.collection.ArraySet
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
+import org.godotengine.godot.plugin.SignalInfo
+import java.util.Date
 
 
 class AndroidGodotPluginTemplate(godot: Godot) : GodotPlugin(godot) {
@@ -14,11 +17,34 @@ class AndroidGodotPluginTemplate(godot: Godot) : GodotPlugin(godot) {
     override fun getPluginMethods(): List<String> {
         return listOf(
             "getHelloWorldNative",
+            "signalTest",
+            "signalTest2",
             "sendSms"
         )
     }
     fun getHelloWorldNative(): String {
         return "Hello World from Android"
+    }
+
+    fun signalTest() {
+        Thread {
+            Log.d("signalTest", "called")
+            Thread.sleep(3000)
+            emitSignal("wake_up")
+        }.start()
+    }
+
+    fun signalTest2() {
+        Thread {
+            runCatching {
+                Log.d("signalTest2", "called")
+                Thread.sleep(3000)
+                val date = Date()
+                emitSignal("wake_up2", date.toString())
+            }.onFailure {
+                Log.e("signalTest2", "error", it)
+            }
+        }.start()
     }
 
     fun sendSms(number: String, message: String): String? {
@@ -29,5 +55,13 @@ class AndroidGodotPluginTemplate(godot: Godot) : GodotPlugin(godot) {
         }catch(exp: Exception){
             exp.message?.toString()
         }
+    }
+
+    override fun getPluginSignals():Set<SignalInfo> {
+        val signals = ArraySet<SignalInfo>()
+        signals.add(SignalInfo("wake_up"))
+        val c = String::class.java
+        signals.add(SignalInfo("wake_up2", c))
+        return signals
     }
 }
